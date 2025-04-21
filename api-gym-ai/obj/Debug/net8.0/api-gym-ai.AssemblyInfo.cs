06d21cdd2927c343ccd6e8636ea8534cb32d571d
StephanieGdSantos <gomess214@gmail.com>
2025-04-21 20:35:13 -0300

    atualiza arquivos internos

--- api-gym-ai/obj/Debug/net8.0/api-gym-ai.AssemblyInfo.cs
+++ api-gym-ai/obj/Debug/net8.0/api-gym-ai.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api-gym-ai")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a00af99f3f90bb6bb707927bf44c64b5de458e1d")]
 [assembly: System.Reflection.AssemblyProductAttribute("api-gym-ai")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api-gym-ai")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

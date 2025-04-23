1c518012fe13aee4523e3b92c2844ce0b95e935b
StephanieGdSantos <gomess214@gmail.com>
2025-04-22 21:21:40 -0300

    feat: adiciona classes para criar plano de treino completo

---
+++ api-gym-ai/Models/Treino.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace api_gym_ai.Models
+{
+    public class Treino
+    {
+        public List<VariacaoDeTreino> VariacaoDeTreino { get; set; }
+        public DateTime DataInicio { get; set; }
+        public DateTime DataFim { get; set; }
+    }
+}
---
+++ api-gym-ai/Models/VariacaoDeTreino.cs
@@ -0,0 +1,9 @@
+﻿namespace api_gym_ai.Models
+{
+    public class VariacaoDeTreino
+    {
+        public string Dia { get; set; } = string.Empty;
+        public string MusculosTrabalhados { get; set; } = string.Empty;
+        public List<Exercicio> Exercicio { get; set; } = new List<Exercicio>();
+    }
+}

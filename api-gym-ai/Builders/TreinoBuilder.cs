b6e3ecc3a03e74439ecd2ebca08b45f3f9a26f5d
StephanieGdSantos <gomess214@gmail.com>
2025-04-23 22:13:09 -0300

    feat: adiciona builder de treino

---
+++ api-gym-ai/Builders/TreinoBuilder.cs
@@ -0,0 +1,43 @@
+﻿using api_gym_ai.Models;
+
+namespace api_gym_ai.Builders
+{
+    public class TreinoBuilder
+    {
+        private readonly Treino _treino = new();
+        public TreinoBuilder ComVariacaoA(VariacaoDeTreino variacaoDeTreino)
+        {
+            _treino.VariacaoDeTreino.Add(variacaoDeTreino);
+            return this;
+        }
+        public TreinoBuilder ComVariacaoB(VariacaoDeTreino variacaoDeTreino)
+        {
+            _treino.VariacaoDeTreino.Add(variacaoDeTreino);
+            return this;
+        }
+        public TreinoBuilder ComVariacaoC(VariacaoDeTreino variacaoDeTreino)
+        {
+            _treino.VariacaoDeTreino.Add(variacaoDeTreino);
+            return this;
+        }
+        public TreinoBuilder ComVariacaoD(VariacaoDeTreino variacaoDeTreino)
+        {
+            _treino.VariacaoDeTreino.Add(variacaoDeTreino);
+            return this;
+        }
+        public TreinoBuilder ComDataInicio(DateTime dataInicio)
+        {
+            _treino.DataInicio = dataInicio;
+            return this;
+        }
+        public TreinoBuilder ComDataFim(DateTime dataFim)
+        {
+            _treino.DataFim = dataFim;
+            return this;
+        }
+        public Treino Build()
+        {
+            return _treino;
+        }
+    }
+}
---
+++ api-gym-ai/Interfaces/ITreinoBuilder.cs
@@ -0,0 +1,14 @@
+﻿using api_gym_ai.Models;
+
+namespace api_gym_ai.Interfaces
+{
+    public interface ITreinoBuilder
+    {
+        public ITreinoBuilder ComVariacaoA(VariacaoDeTreino variacaoDeTreino);
+        public ITreinoBuilder ComVariacaoB(VariacaoDeTreino variacaoDeTreino);
+        public ITreinoBuilder ComVariacaoC(VariacaoDeTreino variacaoDeTreino);
+        public ITreinoBuilder ComVariacaoD(VariacaoDeTreino variacaoDeTreino);
+        public ITreinoBuilder ComDataInicio(DateTime dataInicio);
+        public ITreinoBuilder ComDataFim(DateTime dataFim);
+    }
+}

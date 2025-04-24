5ff10d7b4ed00e9262383319f249ab1662c39100
StephanieGdSantos <gomess214@gmail.com>
2025-04-23 22:14:59 -0300

    feat: adiciona criação de treinos

---
+++ api-gym-ai/Facades/TreinoFacade.cs
@@ -0,0 +1,46 @@
+﻿using api_gym_ai.Builders;
+using api_gym_ai.Models;
+
+namespace api_gym_ai.Facades
+{
+    public static class TreinoFacade
+    {
+        public static Treino? MontarTreino(string retornoChat)
+        {
+            var treinoProposto = PromptFacade.ExtrairRespostaDoChat(retornoChat);
+            var variacaoDeTreino = VariacaoDeTreinoFacade.ListarVariacaoDeTreinos(treinoProposto);
+
+            var quantidadeVariacoes = variacaoDeTreino.Count;
+
+            switch (quantidadeVariacoes)
+            {
+                case 2:
+                    return new TreinoBuilder()
+                        .ComVariacaoA(variacaoDeTreino[0])
+                        .ComVariacaoB(variacaoDeTreino[1])
+                        .ComDataInicio(DateTime.Now)
+                        .ComDataFim(DateTime.Now.AddDays(120))
+                        .Build();
+                case 3:
+                    return new TreinoBuilder()
+                        .ComVariacaoA(variacaoDeTreino[0])
+                        .ComVariacaoB(variacaoDeTreino[1])
+                        .ComVariacaoC(variacaoDeTreino[2])
+                        .ComDataInicio(DateTime.Now)
+                        .ComDataFim(DateTime.Now.AddDays(120))
+                        .Build();
+                case 4:
+                    return new TreinoBuilder()
+                        .ComVariacaoA(variacaoDeTreino[0])
+                        .ComVariacaoB(variacaoDeTreino[1])
+                        .ComVariacaoC(variacaoDeTreino[2])
+                        .ComVariacaoD(variacaoDeTreino[3])
+                        .ComDataInicio(DateTime.Now)
+                        .ComDataFim(DateTime.Now.AddDays(120))
+                        .Build();
+            }
+
+            return null;
+        }
+    }
+}
---
+++ api-gym-ai/Facades/VariacaoDeTreinoFacade.cs
@@ -0,0 +1,35 @@
+﻿using api_gym_ai.Builders;
+using api_gym_ai.Models;
+
+namespace api_gym_ai.Facades
+{
+    public static class VariacaoDeTreinoFacade
+    {
+        public static List<VariacaoDeTreino> ListarVariacaoDeTreinos(string retornoChat)
+        {
+            var variacaoDeTreinosSplit = retornoChat
+                .Split('|')
+                .ToList();
+
+            var listaVariacaoDeTreinos = new List<VariacaoDeTreino>();
+            foreach (var treino in variacaoDeTreinosSplit)
+            {
+                var treinoSplit = treino
+                    .Split('-')
+                    .ToList();
+
+                var exerciciosDoTreino = ExercicioFacade.ListarExerciciosPropostos(treinoSplit[1]);
+
+                var treinoProposto = new VariacaoDeTreino(
+                    treinoSplit[0],
+                    exerciciosDoTreino,
+                    treinoSplit[2]
+                );
+
+                listaVariacaoDeTreinos.Add(treinoProposto);
+            }
+
+            return listaVariacaoDeTreinos;
+        }
+    }
+}

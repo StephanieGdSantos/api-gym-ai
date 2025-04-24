3f2dffa03f2a6aa6fe2afd871d0915c0b8a8e04a
StephanieGdSantos <gomess214@gmail.com>
2025-04-23 22:15:35 -0300

    feat: adiciona construtores

--- api-gym-ai/Models/Exercicio.cs
+++ api-gym-ai/Models/Exercicio.cs
@@ -8,5 +8,17 @@ namespace api_gym_ai.Models
         public int Series { get; set; }
         public string Repeticoes { get; set; } = string.Empty;
         public IEnumerable<string>? MusculoAlvo { get; set; }
+
+        public Exercicio()
+        {
+        }
+
+        public Exercicio(string nome, int series, string repeticoes, IEnumerable<string>? musculoAlvo)
+        {
+            Nome = nome;
+            Series = series;
+            Repeticoes = repeticoes;
+            MusculoAlvo = musculoAlvo;
+        }
     }
 }
--- api-gym-ai/Models/VariacaoDeTreino.cs
+++ api-gym-ai/Models/VariacaoDeTreino.cs
@@ -5,5 +5,12 @@
         public string Dia { get; set; } = string.Empty;
         public string MusculosTrabalhados { get; set; } = string.Empty;
         public List<Exercicio> Exercicio { get; set; } = new List<Exercicio>();
+
+        public VariacaoDeTreino(string dia, List<Exercicio> exercicio, string musculosTrabalhados)
+        {
+            Dia = dia;
+            Exercicio = exercicio;
+            MusculosTrabalhados = musculosTrabalhados;
+        }
     }
 }

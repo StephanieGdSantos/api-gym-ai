c742b6f4802c4430da2097ebfcafdec95c6e09c1
StephanieGdSantos <gomess214@gmail.com>
2025-04-27 18:18:23 -0300

    feat: adiciona containers para injeção de dependencias

--- api-gym-ai/Program.cs
+++ api-gym-ai/Program.cs
@@ -1,9 +1,21 @@
-using api_gym_ai.Interfaces;
+using api_gym_ai.Builders;
+using api_gym_ai.Facades;
+using api_gym_ai.Interfaces.Adapters;
+using api_gym_ai.Interfaces.Builders;
+using api_gym_ai.Interfaces.Services;
 using api_gym_ai.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+builder.Services.AddScoped<ITreinoAdapter, TreinoAdapter>();
+builder.Services.AddScoped<IPromptAdapter, PromptAdapter>();
+builder.Services.AddScoped<IVariacaoDeTreinoAdapter, VariacaoDeTreinoAdapter>();
+builder.Services.AddScoped<IExercicioAdapter, ExercicioAdapter>();
+builder.Services.AddScoped<ICohereService, CohereService>();
+builder.Services.AddScoped<IExercicioBuilder, ExercicioBuilder>();
+builder.Services.AddScoped<ITreinoBuilder, TreinoBuilder>();
+builder.Services.AddScoped<IPromptBuilder, PromptBuilder>();
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle

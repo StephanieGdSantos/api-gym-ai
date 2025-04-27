8a915b63ceeb5d653fc8058b881f737a718241d2
StephanieGdSantos <gomess214@gmail.com>
2025-04-27 18:23:36 -0300

    fix: arruma retornos do builder

--- api-gym-ai/Builders/TreinoBuilder.cs
+++ api-gym-ai/Builders/TreinoBuilder.cs
@@ -1,36 +1,43 @@
-﻿using api_gym_ai.Models;
+﻿using api_gym_ai.Interfaces.Builders;
+using api_gym_ai.Models;
 
 namespace api_gym_ai.Builders
 {
-    public class TreinoBuilder
+    public class TreinoBuilder : ITreinoBuilder
     {
         private readonly Treino _treino = new();
-        public TreinoBuilder ComVariacaoA(VariacaoDeTreino variacaoDeTreino)
+
+        public TreinoBuilder()
+        {
+            _treino.VariacaoDeTreino = new List<VariacaoDeTreino>();
+        }
+
+        public ITreinoBuilder ComVariacaoA(VariacaoDeTreino variacaoDeTreino)
         {
             _treino.VariacaoDeTreino.Add(variacaoDeTreino);
             return this;
         }
-        public TreinoBuilder ComVariacaoB(VariacaoDeTreino variacaoDeTreino)
+        public ITreinoBuilder ComVariacaoB(VariacaoDeTreino variacaoDeTreino)
         {
             _treino.VariacaoDeTreino.Add(variacaoDeTreino);
             return this;
         }
-        public TreinoBuilder ComVariacaoC(VariacaoDeTreino variacaoDeTreino)
+        public ITreinoBuilder ComVariacaoC(VariacaoDeTreino variacaoDeTreino)
         {
             _treino.VariacaoDeTreino.Add(variacaoDeTreino);
             return this;
         }
-        public TreinoBuilder ComVariacaoD(VariacaoDeTreino variacaoDeTreino)
+        public ITreinoBuilder ComVariacaoD(VariacaoDeTreino variacaoDeTreino)
         {
             _treino.VariacaoDeTreino.Add(variacaoDeTreino);
             return this;
         }
-        public TreinoBuilder ComDataInicio(DateTime dataInicio)
+        public ITreinoBuilder ComDataInicio(DateTime dataInicio)
         {
             _treino.DataInicio = dataInicio;
             return this;
         }
-        public TreinoBuilder ComDataFim(DateTime dataFim)
+        public ITreinoBuilder ComDataFim(DateTime dataFim)
         {
             _treino.DataFim = dataFim;
             return this;

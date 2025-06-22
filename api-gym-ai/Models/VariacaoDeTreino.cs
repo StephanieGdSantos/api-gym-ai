24d01563ba466b677d58d27d8c42615683fe2720
StephanieGdSantos <gomess214@gmail.com>
2025-06-21 21:58:03 -0300

    refactor: deixa tipagem generica

--- api-gym-ai/Models/VariacaoDeTreino.cs
+++ api-gym-ai/Models/VariacaoDeTreino.cs
@@ -3,10 +3,10 @@
     public class VariacaoDeTreino
     {
         public string Dia { get; set; } = string.Empty;
-        public string MusculosTrabalhados { get; set; } = string.Empty;
-        public List<Exercicio> Exercicio { get; set; } = new List<Exercicio>();
+        public IEnumerable<string> MusculosTrabalhados { get; set; }
+        public IEnumerable<Exercicio> Exercicio { get; set; }
 
-        public VariacaoDeTreino(string dia, List<Exercicio> exercicio, string musculosTrabalhados)
+        public VariacaoDeTreino(string dia, IEnumerable<Exercicio> exercicio, IEnumerable<string> musculosTrabalhados)
         {
             Dia = dia;
             Exercicio = exercicio;

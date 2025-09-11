c5d3bec7d59d737f5a2a84ae93e4b7681522264b
StephanieGdSantos <gomess214@gmail.com>
2025-09-10 23:26:11 -0300

    feat: adiciona retorno genérico para posterior formatação

--- API.GymAi/Models/Resposta.cs
+++ API.GymAi/Models/Resposta.cs
@@ -5,7 +5,6 @@ namespace API.GymAi.Models
     public class Resposta<T>
     {
         public T? Dados { get; set; }
-        public string? Mensagem { get; set; }
         public bool Sucesso { get; set; }
         public HttpStatusCode StatusCode { get; set; }
     }
